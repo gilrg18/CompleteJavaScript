4f1a895b01c79742031c14721142c62a8c97c706
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-08-10 19:26:03 -0700

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,23 @@
 let js = "amazing";
-if (js === "amazing") alert("yay");
+//if (js === "amazing") alert("yay");
+
+//declaring a varibale 'firstName' with value 'Gil'
+let firstName = "Gil";
+let lastName = "Rogel";
+console.log(firstName + " " + lastName);
+
+//ilegal variable name '1name' 'first&last'
+//let 1name = 'gil';
+//variables can only contain numbers,letters,_,$
+//you cannot use reserved js keywords as variable names:
+//f.e: new, let, var, etc..
+let $1_gil = "gil";
+console.log($1_gil);
+
+//contants are written in UPPERCASE (conventionally)
+let PI = 3.1416;
+
+//variable names should be DESCRIPTIVE
+let myFirstJob = "Support";
+let myCurrentJob = "Dj";
+//instead of job1, job2, etc...
