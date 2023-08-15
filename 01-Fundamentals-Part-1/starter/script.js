90362fba61d3d2379bb159f2e11eaebfb8c7b18e
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-08-14 17:44:02 -0700

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -28,3 +28,21 @@ let continent = "America";
 let population = 126700000;
 
 console.log(country + " " + continent + " " + population);
+
+//DATA TYPES
+//Values is either an object or a primitive value
+//7 primitive data types: number, string, boolean,
+//undefined (empty value, declared but not defined),
+//null
+//symbol(unique value thath cannot be changed (ES2015))
+//BigInt(ES2020): Larger itnegers than the number type can hold
+//JS has dynamic typing: automatically determines the data type when declaring a variable
+console.log(typeof true); //"boolean"
+console.log(typeof 27); //"number"
+console.log(typeof "Gil"); //"string"
+let year;
+console.log(year); //undefined
+console.log(typeof year); //"undefined"
+year = 2023;
+console.log(typeof year); //"number" (result of js dynamic typing)
+console.log(typeof null); //object (which is a bug, it should be null)
