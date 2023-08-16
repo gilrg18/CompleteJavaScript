49cc6201b81d49673a06849337354f36b3d4928c
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-08-16 16:34:11 -0700

    Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 //select a class with .
 //select and id with #
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🥳 Correct Number! ';
@@ -9,3 +10,17 @@ document.querySelector('.score').textContent = 69;
 
 document.querySelector('.guess').value = 111;
 console.log(`value: ${document.querySelector('.guess').value}`);
+*/
+
+//HANDLING CLICK EVENTS
+// const x = function () {
+//   console.log(document.querySelector('.guess').value);
+// };
+// document.querySelector('.check').addEventListener('click', x);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(`Guess: ${guess} ${typeof guess}`);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number ';
+  }
+});
