cc3b45c4ef8a2c72fd09cb68f2796e2b81874970
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-08-16 16:24:14 -0700

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,3 +2,10 @@
 //select a class with .
 //select and id with #
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🥳 Correct Number! ';
+document.querySelector('.number').textContent = 42;
+document.querySelector('.score').textContent = 69;
+
+document.querySelector('.guess').value = 111;
+console.log(`value: ${document.querySelector('.guess').value}`);
