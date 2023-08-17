24cfbcdb0572a44328de1fa6ff1d69c91ca5a43c
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-08-17 15:44:02 -0700

    Handling 'Esc' Keypress event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -7,7 +7,7 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log('Button clicked');
+  //console.log('Button clicked');
   //remove the 'hidden' class from the modal so it can be shown when
   //the button is clicked
   modal.classList.remove('hidden');
@@ -27,3 +27,13 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//keyboard events are also global events because they do not
+//happen on one specific element, they usually happen on the whole document
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
