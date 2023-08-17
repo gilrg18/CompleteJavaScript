1c11c532e8ed7818a571b711446ffbdc6f63e703
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-08-16 17:12:06 -0700

    Manipulating CSS Styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,7 +15,8 @@ console.log(`value: ${document.querySelector('.guess').value}`);
 //IMPLEMENTING THE GAME LOGIC
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+console.log(`Secret Number: ${secretNumber}`);
+//document.querySelector('.number').textContent = secretNumber;
 //HANDLING CLICK EVENTS
 // const x = function () {
 //   console.log(document.querySelector('.guess').value);
@@ -29,6 +30,10 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number! ';
     document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.check').disabled = true;
+    //MANIPULATING CSS STYLES
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High! ';
@@ -38,6 +43,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You Lost! ';
       document.querySelector('.score').textContent = 0;
       document.querySelector('.check').disabled = true;
+      document.querySelector('body').style.backgroundColor = 'red';
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
@@ -48,6 +54,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You Lost! ';
       document.querySelector('.score').textContent = 0;
       document.querySelector('.check').disabled = true;
+      document.querySelector('body').style.backgroundColor = 'red';
     }
   }
 });
