ed65a1c95e6a55b55173a3dc23a167f4893bdc15
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-08-21 15:20:51 -0700

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,3 +10,22 @@ if (hasDriversLicense) console.log("I can drive");
 
 //strict mode reserved word 'interface'
 //const interface = "audio";
+
+//FUNCTIONS
+function logger() {
+  console.log("GG WP");
+}
+//calling/running/invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+console.log(fruitProcessor(1, 2));
+let appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const num = Number("23");
