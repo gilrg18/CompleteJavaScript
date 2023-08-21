8e641744b1f458bf2ca04c930dece87e7d295277
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-08-21 15:28:12 -0700

    Function Declaration and Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -29,3 +29,25 @@ let appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
 
 const num = Number("23");
+
+//FUNCTION DECLARATIONS VS FUNCTION EXPRESSIONS
+
+//Declaration
+function calcAge1(birthYear) {
+  return 2023 - birthYear;
+}
+
+console.log(calcAge1(1996));
+const age1 = calcAge1(1996);
+
+//Expression
+//function without a name is an anonymous function
+//expressions produce values
+const calcAge2 = function (birthYear) {
+  return 2023 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+//you can call function declarations BEFORE they are declared (hoisting)
+//you cant do that with function expressions
