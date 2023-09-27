0d49a60d595bc86fd443e62537d79a20fe462071
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-09-27 13:00:41 -0700

    Challenge - Objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -268,3 +268,38 @@ console.log(gilObject.age);
 console.log(gilObject.age);
 
 console.log(gilObject.getSummary());
+
+//CHALLENGE
+/* Write your code below. Good luck! 🙂 */
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const markBMI = mark.calcBMI();
+const johnBMI = john.calcBMI();
+if (markBMI > johnBMI) {
+  console.log(
+    `${mark.fullName}'s BMI (${markBMI}) is higher than ${john.fullName}'s (${johnBMI})!`
+  );
+} else {
+  console.log(
+    `${john.fullName}'s BMI (${johnBMI}) is higher than ${mark.fullName}'s (${markBMI})!`
+  );
+}
