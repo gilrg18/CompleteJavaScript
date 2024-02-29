d5904ed8a12e023eba7ab1243bddba2b0b84168c
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-02-29 13:16:33 -0700

    Rest Pattern and Parameters Practice

--- 09-Data-Structures-Operators/starter/practice.js
+++ 09-Data-Structures-Operators/starter/practice.js
@@ -231,3 +231,16 @@ function spellWord (str) {
 }
 
 spellWord('JavaScript')
+
+//REST PATTERN AND PARAMETERS
+const [mainKeyword, ...rest] = books[0].keywords
+console.log(mainKeyword, rest);
+
+const{publisher: bookPublisher, ...restOfTheBook} = books[1];
+console.log(bookPublisher, restOfTheBook);
+
+function printBookAuthorsCount(title, ...authors){
+  console.log(`The book "${title}" has ${authors.length} authors`);
+}
+
+printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
