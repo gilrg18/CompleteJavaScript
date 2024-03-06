85efa8e602c12e0ebd5186bde9249214f402256f
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-06 16:00:06 -0700

    FUNCTIONS - Default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//DEFAULT PARAMETERS
+const bookings = [];
+//ES6 - Default values can contain an expression
+//Order matters, we cannot call numPassengers before eg(price=199*numPassengers,numPassengers=1)
+const createBooking = function(flightNum, numPassengers=1, price=199*numPassengers){
+    //ES5
+    //numPassengers = numPassengers || 1;
+    //price = price || 199;
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking)
+}
+
+createBooking('LH123', 5);
+createBooking('AB436');
+createBooking('XX144', 10,999);
+createBooking('XX144', undefined, undefined);//undefined takes the default value
