2727e87339df96bec615b06323163b391198d877
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-03-08 16:22:53 -0700

    Functions accepting CALLBACK functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -119,3 +119,37 @@ function count(){//Higher order function
     }
 }
 
+
+//FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+const oneWord = function(str){
+    return str.replaceAll(' ','').toLowerCase();
+}
+
+const upperFirstWord = function(str){
+    const [first, ...others] = str.split(' ');
+    console.log(first, others);
+    return [first.toUpperCase(), ...others].join(' ');
+}
+//higher order function (takes a function as a parameter) fn is the callback function
+const transformer = function(str, fn){
+    console.log(`Original String: ${str}`);
+    console.log(`Transformed String: ${fn(str)}`);
+
+    console.log(`Transformed By : ${fn.name}`);
+}
+//console.log(oneWord('abc dsd'));
+//console.log(upperFirstWord('abc asd asd asd'));
+//We are not calling the function upperFirstWord (upperFirstWord()), 
+//we are just passing it as a parameter(callback function)
+transformer('JavaScript is confusing lol', upperFirstWord);
+transformer('JavaScript is confusing lol', oneWord);
+
+const high5 = function (){
+    console.log('😁');
+}
+//JS uses callbacks all the time
+//high5 is the callback function and addEventListener is the Higher-order function
+//document.body.addEventListener('click', high5);
+
+//ABSTRACTION means hiding implementation/details because we dont really care about it
+//for example the transformer function doesnt care about how the callback functions passed to it are implemented
